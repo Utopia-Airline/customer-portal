7699c3b1e95846def2219f4a461eac8260c998e8
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-03-16 15:38:58 -0400

    resolve merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./styles/App.scss";
-import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Switch, Route} from 'react-router-dom';
 import Flights from './components/flights/Flights';
 import Home from './components/home/Home';
 import {Provider} from "react-redux";
@@ -9,17 +9,17 @@ import ProfilePage from "./pages/ProfilePage";
 
 
 const App = () => {
-    return (
-      <Provider store={store}>
-        {
-          <Router>
-           <Switch>
-
-              <Route exact={true} path="/home" component={Home} / >
-              <Route exact={true} path="/"> <Redirect to="/home" /> </Route>
-              <Route exact={true} path="/flights" component={Flights} />
-
-              {/* <Route path="/login" component={Login} />
+  return (
+    <Provider store={store}>
+      {
+        <Router>
+          <Switch>
+            <Route exact={true} path="/home" component={Home}/>
+            <Route path="/myaccount" component={ProfilePage}/>
+            <Route path="/login" component={LoginPage}/>
+            <Route exact={true} path="/"> <Redirect to="/home"/> </Route>
+            <Route exact={true} path="/flights" component={Flights}/>
+            {/*
               <Route path="/signup" component={Signup} />
               <Route exact={true} path="/bookings" component={Bookings} />
               <Route path='bookings/add' component={BookingAdd} />
@@ -27,19 +27,16 @@ const App = () => {
               <Route exact={true} path='users' component={users} />
               <Route path='users/add' component={UserCreation} />
               <Route path='users/:id' component={UserById} />  */}
-            </Switch>
-          </Router>
-        }
-
-
-        {/* <Link to="/" >Home</Link>
+          </Switch>
+        </Router>
+      }
+      {/* <Link to="/" >Home</Link>
         <Link to='/Login'>Login</Link>
         <Link to='/Booking' >Booking</Link>
         <Link to='/Flights' >Flights</Link>
         <Link to='/' ></Link>
         <Link to='/' >Home</Link> */}
-
-      </Provider>
-    )
+    </Provider>
+  )
 }
 export default App;

e4be8f80faa5fbb72c834760ef984837ef719f10
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2021-03-24 14:25:16 -0400

    fix type make header typescript friendly

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,9 +4,10 @@ import {useHistory, Link} from 'react-router-dom'
 import {connect} from "react-redux";
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import {logout} from "../store/auth/actions";
+import User from "../models/User";
 
-const Header = ({dispatch, isLoggedIn, user}) => {
-    const history = useHistory();
+const Header = ({dispatch, isLoggedIn, user, hasErrors, loading}: UserProps) => {
+  const history = useHistory();
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
@@ -18,32 +19,41 @@ const Header = ({dispatch, isLoggedIn, user}) => {
           <Link className="nav-link" to='/flights'>Flights</Link>
 
         </Nav>
-        { user && <Nav>
-          <NavDropdown title={user.username} alignRight id="collasible-nav-dropdown">
+        {user && <Nav>
+          <NavDropdown title={user.username} alignRight id="collapsible-nav-dropdown">
             <NavDropdown.Item as={Link} to='/myAccount'>My Account</NavDropdown.Item>
             <NavDropdown.Item as={Link} to='/bookings'>My Bookings</NavDropdown.Item>
-            <NavDropdown.Item 
-                onClick={() => {
-                    dispatch(logout(process.env["REACT_APP_SESSION_URL"])).then(res => {
-                        if (res)
-                            history.push('/home');
-                        else
-                            history.push('/home');
-                    })
-            }}>Logout</NavDropdown.Item>
+            <NavDropdown.Item
+              onClick={() => {
+                dispatch(logout(process.env["REACT_APP_SESSION_URL"])).then(res => {
+                  if (res)
+                    history.push('/home');
+                  else
+                    history.push('/home');
+                })
+              }}>Logout</NavDropdown.Item>
             <NavDropdown.Divider/>
             {/*<NavDropdown.Item onClick>logout</NavDropdown.Item>*/}
           </NavDropdown>
-        </Nav> }
-        { !user && <Button className="btn btn-secondary btn-sm" type="button">
-            <Link className="nav-link" to='/login'>Login</Link>
-            </Button>}
+        </Nav>}
+        {!user && <Button className="btn btn-secondary btn-sm" type="button">
+          <Link className="nav-link" to='/login'>Login</Link>
+        </Button>}
       </Navbar.Collapse>
     </Navbar>
   )
 }
+
+interface UserProps {
+  dispatch?: Function;
+  loading?: boolean;
+  user?: User;
+  hasErrors?: boolean;
+  isLoggedIn: boolean;
+}
+
 const mapStateToProps = state => ({
-    isLoggedIn: state.auth.isLoggedIn,
-    user: state.auth.user
-  });
+  isLoggedIn: state.auth.isLoggedIn,
+  user: state.auth.user
+});
 export default connect(mapStateToProps)(Header);
